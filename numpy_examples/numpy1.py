d344eff64bd3009ec799398999756a2bf798e894
NareshTirunagari <162067560+NareshTirunagari@users.noreply.github.com>
2025-06-26 21:59:49 -0700

    numpy sample examples

---
+++ numpy_examples/numpy1.py
@@ -0,0 +1,19 @@
+import numpy as np
+# pip install numpy
+
+arr1 = np.array([100, 78, 65, 45], dtype='int8')
+
+#data type of the array
+print(type(arr1))
+
+#number of dimensions in the array
+print(arr1.ndim)
+
+#size of the array?
+print(arr1.size)
+
+#shape of the array
+print(arr1.shape)
+
+#data type of the elements
+print(arr1.dtype)
