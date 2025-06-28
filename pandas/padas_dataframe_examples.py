9f01c4c15a8f2d8ae3595b5b1d79337c340a27aa
NareshTirunagari <162067560+NareshTirunagari@users.noreply.github.com>
2025-06-28 16:40:40 -0700

    pandas dataframe examples

--- pandas/padas_dataframe_examples.py
+++ pandas/padas_dataframe_examples.py
@@ -7,3 +7,18 @@ print('\n\nprinting df info:\n')
 print(df.info())
 print('\n\nprinting df description:\n')
 print(df.describe())
+
+print('\n')
+print(df.head(n=3))
+
+print('\n')
+print(df.tail(n=2))
+
+print('\nshape:')
+print(df.shape)
+
+print('\n columns:')
+print(df.columns)
+
+print('\n list of columns:')
+print(list(df.columns))
